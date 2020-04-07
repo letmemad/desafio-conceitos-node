102b7d6bd8fa7a0c13b51939ddc72ca8cb2c4c0a
sreliaas@gmail.com <Elias Souza>
2020-04-07 12:31:37 -0300

    Upload

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,61 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository)
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repository = repositories.find(element => element.id === id)
+  if(repository === undefined) return response.status(400).json({ 
+    error: "Repository not found."
+   })
+
+   const { title, url, techs } = request.body
+
+   repository.title = title
+   repository.url = url
+   repository.techs = techs
+
+  return response.json(repository)
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params
+  const repository = repositories.findIndex(e => e.id === id)
+  if(repository < 0) return response.status(400).json({
+    error: "Repository not found."
+   })
+
+  repositories.splice(repository.id, 1)
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+    const { id } = request.params
+    const repository = repositories.find(element => element.id === id)
+    if(repository === undefined) return response.status(400).json({ 
+      error: "Repository not found."
+    })
+
+    repository.likes = repository.likes + 1
+    return response.json(repository)
 });
 
 module.exports = app;
